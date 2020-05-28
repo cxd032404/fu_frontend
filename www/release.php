17ac0a32649a6bc5ed49638a7524eb5eb0358591
mijianghao7758 <mjh329567384@sina.com>
2020-05-28 14:54:46 +0800

    meige

---
+++ www/release.php
@@ -0,0 +1,8 @@
+<?php
+$command = "git checkout master && git status && git pull";
+(exec($command,$return));
+echo implode("\n",$return)."\n";
+unset($return);
+$command = "cd config && cp indexOnline.js index.js";
+(exec($command,$return));
+echo implode("\n",$return)."\n";
